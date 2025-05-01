46e0bb07bc6c21b4e08e4c3c651ecf8e0cae9c82
bnunamak <brennan@nunamaker.de>
2025-05-01 14:48:14 +0200

    feat: compile to wasm / wasi

--- pkg/compile/morphe_compile_config.go
+++ pkg/compile/morphe_compile_config.go
@@ -1,6 +1,8 @@
 package compile
 
 import (
+	"path"
+
 	r "github.com/kalo-build/morphe-go/pkg/registry"
 	rcfg "github.com/kalo-build/morphe-go/pkg/registry/cfg"
 	"github.com/kalo-build/plugin-morphe-psql-types/pkg/compile/cfg"
@@ -47,4 +49,62 @@ func (config MorpheCompileConfig) Validate() error {
 	}
 
 	return nil
+}
+
+func DefaultMorpheCompileConfig(
+	yamlRegistryPath string,
+	baseOutputDirPath string,
+) MorpheCompileConfig {
+	return MorpheCompileConfig{
+		MorpheLoadRegistryConfig: rcfg.MorpheLoadRegistryConfig{
+			RegistryEnumsDirPath:      path.Join(yamlRegistryPath, "enums"),
+			RegistryModelsDirPath:     path.Join(yamlRegistryPath, "models"),
+			RegistryStructuresDirPath: path.Join(yamlRegistryPath, "structures"),
+			RegistryEntitiesDirPath:   path.Join(yamlRegistryPath, "entities"),
+		},
+		MorpheConfig: cfg.MorpheConfig{
+			MorpheModelsConfig: cfg.MorpheModelsConfig{
+				Schema:       "public",
+				UseBigSerial: false,
+			},
+			MorpheEnumsConfig: cfg.MorpheEnumsConfig{
+				Schema:       "public",
+				UseBigSerial: false,
+			},
+			MorpheStructuresConfig: cfg.MorpheStructuresConfig{
+				Schema:            "public",
+				UseBigSerial:      false,
+				EnablePersistence: true,
+			},
+			MorpheEntitiesConfig: cfg.MorpheEntitiesConfig{
+				Schema:         "public",
+				ViewNameSuffix: "_entities",
+			},
+		},
+
+		RegistryHooks: r.LoadMorpheRegistryHooks{},
+
+		EnumWriter: &MorpheTableFileWriter{
+			TargetDirPath: path.Join(baseOutputDirPath, "enums"),
+		},
+		EnumHooks: hook.CompileMorpheEnum{},
+
+		ModelWriter: &MorpheTableFileWriter{
+			TargetDirPath: path.Join(baseOutputDirPath, "models"),
+		},
+		ModelHooks: hook.CompileMorpheModel{},
+
+		EntityWriter: &MorpheViewFileWriter{
+			TargetDirPath: path.Join(baseOutputDirPath, "entities"),
+		},
+		EntityHooks: hook.CompileMorpheEntity{},
+
+		WriteTableHooks: hook.WritePSQLTable{},
+		WriteViewHooks:  hook.WritePSQLView{},
+
+		StructureWriter: &MorpheTableFileWriter{
+			TargetDirPath: path.Join(baseOutputDirPath, "structures"),
+		},
+		StructureHooks: hook.CompileMorpheStructure{},
+	}
 }
